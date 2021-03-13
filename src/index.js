b8781131a8668552a36aac2d1d19dd88f1158863
veronmolosh <nikabu17@gmail.com>
2021-03-13 16:30:32 +0300

    feat: add solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let nAbs = Math.abs(n); //делаем модуль числа n
+    return Number(String(nAbs).split('').reverse().join(''));
 }
